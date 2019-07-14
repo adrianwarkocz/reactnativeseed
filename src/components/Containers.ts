da2f8b14c21f55007c8d9583ebfd49840ec7d6db
Adrian Warkocz <awarkocz@MacBook-Pro-Espeo-5.local>
2019-07-14 12:48:48 +0200

    styled-components

---
+++ src/components/Containers.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+
+export const CentredSafeAreaView = styled.SafeAreaView`
+  marginLeft: 30;
+  marginRight: 30;
+  marginTop: 30;
+`
---
+++ src/components/Texts.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native'
+
+const TextWithMargin = styled.Text`
+  marginTop: 10;
+`
+
+export const PalevioletredText = styled(TextWithMargin)`
+  color: palevioletred;
+`
+
+export const BlueText = styled(TextWithMargin)`
+  color: blue;
+`
+
+export const TitleText = styled.Text`
+  fontSize: 24;
+  color: black;
+  fontWeight: 600;
+`
+
